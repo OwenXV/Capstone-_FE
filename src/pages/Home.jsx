3721e92ab814a221eb09cfd6b8bbbc43c8ba8b33
OwenXV <owensanbuenaventura@hotmail.com>
2023-04-30 08:00:31 +0800

    updated navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { Button } from 'bootstrap'
 import { Container, Form } from 'react-bootstrap'
 import { Navbar, Nav } from 'react-bootstrap'
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import brandicon from '../assets/brandicon.png'
 
 
 const Home = () => {
@@ -11,7 +12,15 @@ const Home = () => {
         {[true && 'sm' && 'md'].map((expand) => (
           <Navbar className="navibar" key={expand} expand={expand}>
             <Container>
-              <Navbar.Brand href="#" className="text-white fs-1">NEPFLIX</Navbar.Brand>
+              <Navbar.Brand href="#" className="text-white d-flex align-items-center">
+                <img
+                  alt=""
+                  src={brandicon}
+                  width="60"
+                  height="60"
+                  />
+                  <h3 className="px-1">BentaPH</h3>
+              </Navbar.Brand>
               <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
               <Navbar.Offcanvas
                 id={`offcanvasNavbar-expand-${expand}`}
@@ -26,11 +35,11 @@ const Home = () => {
                 </Offcanvas.Header>
                 <Offcanvas.Body>
                   <Nav className="justify-content-end flex-grow-1 pe-3 text-white">
-                    <Nav.Link href="/Home"className="text-white fs-5">Home</Nav.Link>
-                    <Nav.Link href="/TVshow" className="text-white fs-5">TV Shows</Nav.Link>
-                    <Nav.Link href="/Movies" className="text-white fs-5">Movies</Nav.Link>
-                    <Nav.Link href="/FAQ" className="text-white fs-5">FAQs</Nav.Link>
-                    <Nav.Link href="/Login" className="text-white fs-5">Login/Sign up</Nav.Link>
+                    <Nav.Link href="/Home"className="text-white fs-5">Categories</Nav.Link>
+                    <Nav.Link href="/Home" className="text-white fs-5">Property</Nav.Link>
+                    <Nav.Link href="/Home" className="text-white fs-5">Register</Nav.Link>
+                    <Nav.Link href="/Home" className="text-white fs-5">Login</Nav.Link>
+                    <Nav.Link href="/Home" className="text-white fs-5">Sell</Nav.Link>
                   </Nav>  
                 </Offcanvas.Body>
               </Navbar.Offcanvas>
