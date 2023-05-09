9fd4e11a010dd6c45ca16966605a63c7ab959328
OwenXV <owensanbuenaventura@hotmail.com>
2023-05-10 02:27:36 +0800

    update

---
+++ src/lib/http.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const http = axios.create({
+    baseURL: import.meta.env.VITE_API,
+    headers:{
+        'Accept': 'application/json',
+    }
+})
+export default http
