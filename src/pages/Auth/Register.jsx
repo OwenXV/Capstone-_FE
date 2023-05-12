7f81593f079dfb7c46fd9cb1ef278d2802510a1f
OwenXV <owensanbuenaventura@hotmail.com>
2023-05-12 14:28:22 +0800

    updates

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -5,209 +5,173 @@ import { useNavigate } from "react-router-dom";
 
 const Register = () => {
   const navigate = useNavigate();
+  const [validated, setValidated] = useState(false);
   const [name, setName] = useState("");
   const [username, setUsername] = useState("");
-  const [picture, setPicture] = useState();
+  const [email, setEmail] = useState("");
   const [address, setAddress] = useState("");
   const [about, setAbout] = useState("");
-  const [email, setEmail] = useState("");
+  const [picture, setPicture] = useState();
   const [password, setPassword] = useState("");
   const [passwordConfirmation, setPasswordConfirmation] = useState("");
-  const [errors, setErrors] = useState({
-    name: [],
-    username: [],
-    address: [],
-    about: [],
-    email: [],
-    picture: [],
-    password: [],
-    passwordConfirmation: [],
-  });
-  const [validated, setValidated] = useState(false);
 
   async function register(e) {
     e.preventDefault();
-    e.stopPropagation();
-
-    setValidated(true);
-
     if (
       !name ||
       !username ||
+      !email ||
       !address ||
       !about ||
-      !email ||
+      !picture ||
       !password ||
       !passwordConfirmation ||
-      !(password === passwordConfirmation)
+      password !== passwordConfirmation
     ) {
+      alert("Invalid Data");
       return;
     }
-    try {
-      const body = {
-        name,
-        username,
-        profile_picture: picture,
-        adress: address,
-        seller_description: about,
-        email,
-        password,
-        password_confirmation: passwordConfirmation,
-      };
-      const res = await http.post("/register", body);
-      localStorage.setItem("user", JSON.stringify(res.data.user));
-      localStorage.setItem("token", res.data.token);
-      navigate("/");
-      navigate(0);
-    } catch (e) {
-      if (e.response.data.errors) {
-        setErrors({
-          name: e.response.data.errors.name ? e.response.data.errors.name : [],
-          email: e.response.data.errors.email
-            ? e.response.data.errors.email
-            : [],
-          password: e.response.data.errors.password
-            ? e.response.data.errors.password
-            : [],
-          passwordConfirmation: e.response.data.errors.password_confirmation
-            ? e.response.data.errors.password_confirmation
-            : [],
-        });
-      } else {
-        alert(e.response.data.message);
-      }
-    }
+    // console.log(
+    //   name,
+    //   username,
+    //   email,
+    //   address,
+    //   about,
+    //   picture,
+    //   password,
+    //   passwordConfirmation
+    // );
+    // let imageName = "";
+    // if (picture) {
+    //   const formData = new formData();
+    //   formData.append("image", picture);
+
+    //   const imgRes = await http.post("/upload", formData);
+    //   image_name = res.data.image_name;
+    // }
+    const body = {
+      name,
+      username,
+      email,
+      adress: address,
+      seller_description: about,
+      profile_picture: picture,
+      password,
+      password_confirmation: passwordConfirmation,
+    };
+
+    const res = await http.post("/register", body);
+
+    localStorage.setItem("user", JSON.stringify(res.data.user));
+    localStorage.setItem("token", res.data.token);
+    navigate("/");
   }
 
   return (
     <div className="RegisterPage">
       <Container className="d-flex justify-content-center py-5">
         <div className="RegisterContainer d-flex align-items-center">
-          <Card style={{ width: "18rem" }} className="RegisterCard">
+          <Card style={{ width: "25rem" }} className="RegisterCard">
             <Card.Body>
+              <h3 className="text-center mb-4">Register to start shopping!</h3>
               <Row>
                 <Col>
                   <Form
                     onSubmit={register}
                     className="RegisterForm align-items-center"
                   >
-                    <Form.Group className="mb-3" controlId="RegisterName">
+                    <Form.Group className="mb-4" controlId="RegisterName">
+                      <Form.Label>Name</Form.Label>
                       <Form.Control
+                        required
                         type="text"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
                         placeholder="Name"
+                        vaue={name}
+                        onChange={(e) => setName(e.target.value)}
                       />
-                      <Form.Control.Feedback type="invalid">
-                        {errors.name.map((error, index) => {
-                          return <p key={index}>{error}</p>;
-                        })}
-                      </Form.Control.Feedback>
+                      <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
                     </Form.Group>
-                    <Form.Group className="mb-3" controlId="RegisterUsername">
+                    <Form.Group className="mb-4" controlId="RegisterUsername">
+                      <Form.Label>Username</Form.Label>
                       <Form.Control
+                        required
                         type="text"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
                         placeholder="Username"
+                        vaue={username}
+                        onChange={(e) => setUsername(e.target.value)}
                       />
-                      <Form.Control.Feedback type="invalid">
-                        {errors.username.map((error, index) => {
-                          return <p key={index}>{error}</p>;
-                        })}
-                      </Form.Control.Feedback>
+                      <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="RegisterAddress">
+                      <Form.Label>Address</Form.Label>
+                      <Form.Control
+                        required
+                        type="text"
+                        placeholder="Street, City, Zipcode"
+                        vaue={address}
+                        onChange={(e) => setAddress(e.target.value)}
+                      />
+                      <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="RegisterPicture">
                       <Form.Control
+                        required
                         type="file"
-                        onChange={(e) => setPicture(e.target.files[0])}
                         placeholder="Profile Picture"
+                        onChange={(e) => setPicture(e.target.files[0])}
                       />
-                      <Form.Control.Feedback type="invalid">
-                        {errors.picture.map((error, index) => {
-                          return <p key={index}>{error}</p>;
-                        })}
-                      </Form.Control.Feedback>
+                      <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
                     </Form.Group>
-                    <Form.Group className="mb-3" controlId="RegisterAddress">
-                      <Form.Control
-                        type="text"
-                        value={address}
-                        onChange={(e) => setAddress(e.target.value)}
-                        placeholder="Street, City, Zipcode"
-                      />
-                      <Form.Control.Feedback type="invalid">
-                        {errors.address.map((error, index) => {
-                          return <p key={index}>{error}</p>;
-                        })}
-                      </Form.Control.Feedback>
-                    </Form.Group>
+
                     <Form.Group className="mb-3" controlId="RegisterAbout">
                       <Form.Control
+                        required
                         type="text"
                         rows="3"
-                        value={about}
-                        onChange={(e) => setAbout(e.target.value)}
                         placeholder="Tell us about yourself"
+                        vaue={about}
+                        onChange={(e) => setAbout(e.target.value)}
                       />
-                      <Form.Control.Feedback type="invalid">
-                        {errors.about.map((error, index) => {
-                          return <p key={index}>{error}</p>;
-                        })}
-                      </Form.Control.Feedback>
+                      <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="RegisterEmail">
                       <Form.Control
+                        required
                         type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
                         placeholder="Enter email"
+                        vaue={email}
+                        onChange={(e) => setEmail(e.target.value)}
                       />
-                      <Form.Control.Feedback type="invalid">
-                        {errors.email.map((error, index) => {
-                          return <p key={index}>{error}</p>;
-                        })}
-                      </Form.Control.Feedback>
+                      <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="RegisterPassword">
                       <Form.Control
+                        required
                         type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
                         placeholder="Password"
+                        vaue={password}
+                        onChange={(e) => setPassword(e.target.value)}
                       />
-                      <Form.Control.Feedback type="invalid">
-                        {errors.password.map((error, index) => {
-                          return <p key={index}>{error}</p>;
-                        })}
-                      </Form.Control.Feedback>
+                      <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
                     </Form.Group>
                     <Form.Group
                       className="mb-3"
                       controlId="RegisterPasswordConfirm"
                     >
                       <Form.Control
+                        required
                         type="password"
-                        value={passwordConfirmation}
+                        placeholder="Confirm Password"
+                        vaue={passwordConfirmation}
                         onChange={(e) =>
                           setPasswordConfirmation(e.target.value)
                         }
-                        placeholder="Confirm Password"
                       />
-                      <Form.Control.Feedback type="invalid">
-                        {errors.passwordConfirmation.map((error, index) => {
-                          return <p key={index}>{error}</p>;
-                        })}
-                      </Form.Control.Feedback>
+                      <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
                     </Form.Group>
                     <Row className="d-flex align-items-center">
                       <Col>
-                        <Button
-                          type="submit"
-                          value="Register"
-                          className="btn-Register"
-                        >
-                          {" "}
+                        <Button type="submit" className="btn-Register">
                           Sign up
                         </Button>
                       </Col>
@@ -222,4 +186,5 @@ const Register = () => {
     </div>
   );
 };
+
 export default Register;
