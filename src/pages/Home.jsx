61393791b9cd2383f5bd5a6fcb18db363e72b175
OwenXV <owensanbuenaventura@hotmail.com>
2023-05-12 16:01:30 +0800

    updated draft of homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,17 @@ import { Pagination } from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+import banner from "../assets/banner.jpeg";
+import product from "../assets/product.jpeg";
+import sale1 from "../assets/sale1.jpg";
+import sale2 from "../assets/sale2.jpg";
+import sale3 from "../assets/sale3.jpg";
 
 const Home = () => {
   return (
     <div>
       <Container>
-        <Row>
+        <Row className="my-4">
           <Col>
             <Swiper
               spaceBetween={30}
@@ -24,19 +29,69 @@ const Home = () => {
               modules={[Pagination]}
               className="mySwiper"
             >
-              <SwiperSlide>Slide 1</SwiperSlide>
-              <SwiperSlide>Slide 2</SwiperSlide>
-              <SwiperSlide>Slide 3</SwiperSlide>
-              <SwiperSlide>Slide 4</SwiperSlide>
-              <SwiperSlide>Slide 5</SwiperSlide>
-              <SwiperSlide>Slide 6</SwiperSlide>
-              <SwiperSlide>Slide 7</SwiperSlide>
-              <SwiperSlide>Slide 8</SwiperSlide>
-              <SwiperSlide>Slide 9</SwiperSlide>
+              <SwiperSlide>
+                <img src={banner} alt="" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src={banner} alt="" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src={banner} alt="" />
+              </SwiperSlide>
             </Swiper>
           </Col>
         </Row>
-        <h3>Feature Categories</h3>
+        <h3>Featured Categories</h3>
+        <Row className="my-4">
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img src={product} />
+              Property 10,000+ listings
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img src={product} />
+              Property 10,000+ listings
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img src={product} />
+              Property 10,000+ listings
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img src={product} />
+              Property 10,000+ listings
+            </Card>
+          </Col>
+        </Row>
+
+        <Row className="my-4">
+          <Col>
+            <Swiper
+              slidesPerView={2}
+              spaceBetween={30}
+              pagination={{
+                clickable: true,
+              }}
+              modules={[Pagination]}
+              className="mySwiper"
+            >
+              <SwiperSlide>
+                <img src={sale1} alt="" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src={sale3} alt="" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src={sale3} alt="" />
+              </SwiperSlide>
+            </Swiper>
+          </Col>
+        </Row>
       </Container>
     </div>
   );
--- src/routes.jsx
+++ src/routes.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Home from "./pages/Home";
 import Register from "./pages/Auth/Register";
 import Login from "./pages/Auth/Login";
+import Profile from "./pages/Profile";
 
 const routes = [
   {
@@ -15,6 +16,10 @@ const routes = [
   {
     path: "/Login",
     element: <Login />,
+  },
+  {
+    path: "/Profile",
+    element: <Profile />,
   },
 ];
 
